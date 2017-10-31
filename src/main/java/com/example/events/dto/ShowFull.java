f9adfc89a8cbe30658b6affdd598368f3c1b7c1d
Frey, Gregor Karl <gregor.karl.frey@sap.com>
2017-10-31 11:30:49 +0100

    exercise_01, step_01: implement show service

--- src/main/java/com/example/events/dto/ShowFull.java
+++ src/main/java/com/example/events/dto/ShowFull.java
@@ -1,5 +1,7 @@
 package com.example.events.dto;
 
+import com.example.events.model.Show;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +24,29 @@ public class ShowFull {
     private String cinemaAddress;
     // Cinema.phone
     private String cinemaPhone;
+
+    /**
+     * Factory method that creates a ShowFull view from a given show.
+     * 
+     * @param show
+     *            input
+     * @return new ShowFull object
+     */
+    public static ShowFull fromShow(Show show) {
+        ShowFull showFull = new ShowFull();
+        showFull.setPrice(show.getPrice());
+        showFull.setDay(show.getDay());
+        showFull.setTime(show.getTime());
+        if (show.getMovie() != null) {
+            showFull.setMovieTitle(show.getMovie().getTitle());
+            showFull.setMovieRating(show.getMovie().getRating());
+        }
+        if (show.getCinema() != null) {
+            showFull.setCinemaName(show.getCinema().getName());
+            showFull.setCinemaAddress(
+                show.getCinema().getZipCode() + " " + show.getCinema().getCity() + ", " + show.getCinema().getStreet());
+            showFull.setCinemaPhone(show.getCinema().getPhone());
+        }
+        return showFull;
+    }
 }
--- src/main/java/com/example/events/service/ShowService.java
+++ src/main/java/com/example/events/service/ShowService.java
@@ -1,19 +1,83 @@
 package com.example.events.service;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import com.example.events.dto.ShowFull;
 import com.example.events.dto.ShowInput;
+import com.example.events.model.Cinema;
+import com.example.events.model.Movie;
+import com.example.events.model.Show;
+import com.example.events.repository.CinemaRepository;
+import com.example.events.repository.MovieRepository;
+import com.example.events.repository.ShowRepository;
 
 public class ShowService {
+    private CinemaRepository cinemaRepository;
+    private MovieRepository movieRepository;
+    private ShowRepository showRepository;
+
+    /**
+     * Construct Show Service with required dependencies.
+     * 
+     * @param cinemaRepository
+     *            {@link CinemaRepository}
+     * @param movieRepository
+     *            {@link MovieRepository}
+     * @param showRepository
+     *            {@link ShowRepository}
+     */
+    public ShowService(CinemaRepository cinemaRepository, MovieRepository movieRepository,
+            ShowRepository showRepository) {
+        this.cinemaRepository = cinemaRepository;
+        this.movieRepository = movieRepository;
+        this.showRepository = showRepository;
+    }
+
+    /**
+     * Save a show from an ShowInput object.
+     * 
+     * @param showInput
+     *            {@link ShowInput}
+     */
     public void save(ShowInput showInput) {
+        Movie movie = movieRepository.findById(showInput.getMovieId());
+        if (movie != null) {
+            Cinema cinema = cinemaRepository.findById(showInput.getCinemaId());
+            if (cinema != null) {
+                Show show = new Show();
+                show.setMovie(movie);
+                show.setCinema(cinema);
+                show.setDay(showInput.getDay());
+                show.setTime(showInput.getTime());
+                show.setPrice(showInput.getPrice());
+                show.setId(showInput.getId());
+                showRepository.save(show);
+            }
+        }
     }
 
+    /**
+     * Find shows corresponding to a movie title.
+     * Matches if the movie title starts with the input string.
+     * @param movieTitle input string
+     * @return all found shows matching‚
+     */
     public Collection<ShowFull> findAllByMovieTitle(String movieTitle) {
-        return null;
+        Collection<Show> shows = showRepository.findAll();
+        return shows.stream().filter(show -> show.getMovie().getTitle().startsWith(movieTitle)).map(ShowFull::fromShow)
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Find shows corresponding to a cinema name.
+     * Matches if the cinema name starts with the input string.
+     * @param cinemaName input string
+     * @return all found shows matching
+     */
     public Collection<ShowFull> findAllByCinemaName(String cinemaName) {
-        return null;
+        Collection<Show> shows = showRepository.findAll();
+        return shows.stream().filter(show -> show.getCinema().getName().startsWith(cinemaName)).map(ShowFull::fromShow)
+                .collect(Collectors.toList());
     }
 }
