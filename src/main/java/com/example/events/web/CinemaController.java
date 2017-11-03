d751ef3fefb468f575237ec1d69ea6f617c70abf
Frey, Gregor Karl <gregor.karl.frey@sap.com>
2017-11-03 15:26:58 +0100

    scaffold CinemaController, MovieController plus test classes

---
+++ src/main/java/com/example/events/web/CinemaController.java
@@ -0,0 +1,39 @@
+package com.example.events.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.events.model.Cinema;
+import com.example.events.repository.CinemaRepository;
+
+@RestController
+@RequestMapping("/cinema")
+public class CinemaController {
+    private CinemaRepository cinemaRepository;
+
+    public CinemaController(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    /**
+     * Create a cinema object in the cinema store.
+     * 
+     * @param cinema
+     *            {@link CinemaRepository}
+     * @return input object
+     */
+    @PostMapping("save")
+    public ResponseEntity<Cinema> save(@RequestBody Cinema cinema) {
+        return null;
+    }
+
+    public Cinema byId(@PathVariable Long id) {
+        return null;
+    }
+}
--- src/test/java/com/example/events/web/TestWebConfig.java
+++ src/test/java/com/example/events/web/TestWebConfig.java
@@ -3,11 +3,17 @@ package com.example.events.web;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 
+import com.example.events.repository.CinemaRepository;
 import com.example.events.service.ShowService;
 
 public class TestWebConfig {
     @Bean
     public ShowService showService() {
         return Mockito.mock(ShowService.class);
+    }
+    
+    @Bean
+    public CinemaRepository cinemaRepository() {
+        return Mockito.mock(CinemaRepository.class);
     }
 }
