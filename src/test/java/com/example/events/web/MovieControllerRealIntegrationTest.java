c68c4611d20a107a095a4a959809325c62d25665
Frey, Gregor Karl <gregor.karl.frey@sap.com>
2017-11-05 18:21:38 +0100

    scaffold cinema and movie controller integration tests

---
+++ src/test/java/com/example/events/web/MovieControllerRealIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.example.events.web;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovieControllerRealIntegrationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testSave() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testById() {
+        fail("Not yet implemented");
+    }
+
+}
