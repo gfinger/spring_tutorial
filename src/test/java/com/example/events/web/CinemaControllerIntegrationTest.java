e2c13e47d66930c177e37d8a1dd27dba64dbc747
Frey, Gregor Karl <gregor.karl.frey@sap.com>
2017-11-05 19:10:09 +0100

    exercise_03, step_01:  integration tests with MockMvc

--- src/test/java/com/example/events/web/CinemaControllerIntegrationTest.java
+++ src/test/java/com/example/events/web/CinemaControllerIntegrationTest.java
@@ -1,24 +1,66 @@
 package com.example.events.web;
 
-import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.events.model.Cinema;
+import com.example.events.repository.CinemaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class CinemaControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SpyBean
+    private CinemaRepository cinemaRepository;
+
+    private Cinema cinema;
 
     @Before
     public void setUp() throws Exception {
+        cinema = new Cinema();
+        cinema.setId(1L);
+        cinema.setName("Luxor");
+        cinema.setCity("Bensheim");
+        cinema.setZipCode("64625");
+        cinema.setStreet("Berliner Ring 26");
     }
 
     @Test
-    public void testSave() {
-        fail("Not yet implemented");
+    public void testSave() throws Exception {
+        cinema.setId(2L);
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(post("/cinema/save").content(objectMapper.writeValueAsString(cinema))
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+        verify(cinemaRepository, times(1)).save(any(Cinema.class));
     }
 
     @Test
-    public void testById() {
-        fail("Not yet implemented");
+    public void testById() throws Exception {
+        mockMvc.perform(get("/cinema/1")).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json"))
+                .andExpect(jsonPath("$.name").value(cinema.getName()));
     }
 
 }
--- src/test/java/com/example/events/web/MovieControllerIntegrationTest.java
+++ src/test/java/com/example/events/web/MovieControllerIntegrationTest.java
@@ -1,24 +1,65 @@
 package com.example.events.web;
 
-import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.events.model.Movie;
+import com.example.events.repository.MovieRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class MovieControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SpyBean
+    private MovieRepository movieRepository;
+
+    private Movie movie;
 
     @Before
     public void setUp() throws Exception {
+        movie = new Movie();
+        movie.setId(1L);
+        movie.setTitle("Blade Runner 2049");
+        movie.setRating(8.5);
     }
 
     @Test
-    public void testSave() {
-        fail("Not yet implemented");
+    public void testSave() throws Exception {
+        movie.setId(2L);
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(
+            post("/movie/save").content(objectMapper.writeValueAsString(movie)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        verify(movieRepository, times(1)).save(any(Movie.class));
     }
 
     @Test
-    public void testById() {
-        fail("Not yet implemented");
+    public void testById() throws Exception {
+        mockMvc.perform(get("/movie/1")).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json"))
+                .andExpect(jsonPath("$.title").value(movie.getTitle()));
     }
 
 }
